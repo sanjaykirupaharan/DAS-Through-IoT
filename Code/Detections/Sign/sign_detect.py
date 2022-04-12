3fd76a5576987ffbb84c9b6a2eb143cb73f33b88
Hirushiharan Thevendran <84900433+hirushiharan@users.noreply.github.com>
2022-04-12 08:30:15 +0530

    Add files via upload

---
+++ Code/Detections/Sign/sign_detect.py
@@ -0,0 +1,196 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import cv2
+import numpy as np
+import torch
+
+from detections.Sign.models.experimental import attempt_load
+from detections.Sign.utils.general import check_img_size, check_suffix, non_max_suppression, scale_coords, set_logging
+from detections.Sign.utils.plots import Annotator
+from detections.Sign.utils.torch_utils import load_classifier, select_device
+#from detections.Database.FirebasePiLocationUpdate import location
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def detect(image,
+           weights='traffic_sign_detection-model.pt',  # model.pt path(s)
+           imgsz=640,  # inference size (pixels)
+           conf_thres=0.25,  # confidence threshold
+           iou_thres=0.45,  # NMS IOU threshold
+           max_det=1000,  # maximum detections per image
+           device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+           classes=None,  # filter by class: --class 0, or --class 0 2 3
+           agnostic_nms=False,  # class-agnostic NMS
+           augment=False,  # augmented inference
+           visualize=False,  # visualize features
+           line_thickness=3,  # bounding box thickness (pixels)
+           half=False,  # use FP16 half-precision inference
+           ):
+
+    # Initialize
+    set_logging()
+    device = select_device(device)
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # color = (0, 255, 255)
+    # text_size = 2
+    # text_thick = 5
+
+    # Load model
+    w = str(weights[0] if isinstance(weights, list) else weights)
+    classify, suffix, suffixes = False, Path(w).suffix.lower(), ['.pt', '.onnx', '.tflite', '.pb', '']
+    check_suffix(w, suffixes)  # check weights have acceptable suffix
+    model = torch.jit.load(w) if 'torchscript' in w else attempt_load(weights, map_location=device)
+    stride = int(model.stride.max())  # model stride
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+    if classify:  # second-stage classifier
+        modelc = load_classifier(name='resnet50', n=2)  # initialize
+        modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    im0 = image
+    img = letterbox(im0, stride=stride)[0]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.parameters())))  # run once
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+        # Inference
+        pred = model(img, augment=augment, visualize=visualize)[0]
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    # p = (int(xyxy[0]), int(xyxy[1] - 3))
+                    for c in det[:, -1].unique():
+                        annotator.box_label(xyxy, color=(0, 255, 255))
+                        # name = str(names[int(c)])
+                        """
+                        if name == names[0]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (12, 15, 13), text_thick)
+                        elif name == names[1]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (80, 0, 15), text_thick)
+                        elif name == names[2]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (1, 55, 105), text_thick)
+                        elif name == names[3]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (12, 10, 11), text_thick)
+                        elif name == names[4]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (10, 20, 30), text_thick)
+                        elif name == names[5]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (69, 63, 78), text_thick)
+                        elif name == names[6]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (2, 90, 255), text_thick)
+                        elif name == names[7]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (255, 0, 6), text_thick)
+                        elif name == names[8]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (47, 18, 81), text_thick)
+                        elif name == names[9]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (26, 28, 97), text_thick)
+                        elif name == names[10]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (7, 156, 90), text_thick)
+                        elif name == names[11]:
+                            annotator.box_label(xyxy, color=color)
+                            # cv2.putText(im0, name, p, cv2.FONT_HERSHEY_SIMPLEX, text_size, (200, 20, 2), text_thick)
+                            """
+                        # location(name)
+
+    img = cv2.resize(im0, (1280, 720))
+    return img
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='traffic_sign_detection-model.pt',
+                        help='model path(s)')
+    parser.add_argument('--source', type=str, default='', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
