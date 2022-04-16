c321fb18cd0068c9ea2b2d29db91b598dc05a51e
Hirushiharan Thevendran <84900433+hirushiharan@users.noreply.github.com>
2022-04-16 20:46:29 +0530

    Add files via upload

---
+++ Code/Detections/pwa-angular-realtime-location-ITE/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  // REPLACE BY YOUR FIREBASE CONFIG HERE
+  firebaseConfig: {
+    apiKey: "<Your API key>",
+    authDomain: "<Your Authentication Domain>",
+    databaseURL: "<Your Database URL>",
+    projectId: "<Your Project Id>",
+    storageBucket: "<>",
+    messagingSenderId: "<>",
+    appId: "<>"
+  }
+};
